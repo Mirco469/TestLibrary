50febf086e1be4dbfc16ad6381f05b4b82d30d4f
Mirco Giardina <mirco.giardina@ennova-research.com>
2021-05-18 18:03:29 +0200

    Added enhancers library tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,971 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TestLibrary';
+
+  model;
+
+  // ACTION PAGER
+  items = [
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    },
+    {
+      "id": 1,
+      "label": "elemento"
+    }
+  ];
+
+  // TABLE
+  configuration = {
+    "headers": [
+      {
+        "id": "id",
+        "displayName": "ID",
+        "width": 50
+      },
+      {
+        "id": "status",
+        "displayName": "Stato",
+        "width": 110
+      },
+      {
+        "id": "priority",
+        "displayName": "Priorità"
+      },
+      {
+        "id": "customer",
+        "displayName": "Cliente"
+      },
+      {
+        "id": "impianto",
+        "displayName": "Impianto"
+      }
+    ],
+    "rowOptions": [
+      {
+        id: 1,
+        hideInMore: false,
+        disabled: false
+      }
+    ]
+  }
+
+  data = [
+    {
+      "id": "0",
+      "customer": "Paul Simon",
+      "impianto": "LOCALE",
+      "priority": "Low",
+      "status": "Pending"
+    },
+    {
+      "id": "1",
+      "customer": "Paul Simon",
+      "impianto": "Barriera articolata contrapposta - parcheggio SUD - esterna DX uscita, con qualche riga di testo in più per mostrare come funziona quando c'è testo in più",
+      "priority": "MYOB",
+      "status": "Pending"
+    },
+    {
+      "id": "2",
+      "customer": "Art Garfunkel",
+      "impianto": "DONE",
+      "priority": "---",
+      "status": "Closed"
+    },
+    {
+      "id": "3",
+      "customer": "Paul Simon",
+      "impianto": "Barriera articolata contrapposta - parcheggio SUD - esterna DX uscita",
+      "priority": "High",
+      "status": "Shipping"
+    }
+  ]
+
+  // SEARCH
+  modelSearch;
+
+  // BREADCRUMB
+  breadcrumbs = [
+    {
+      "id": 4,
+      "label": "Breadcrumb 1"
+    },
+    {
+      "id": 2,
+      "label": "Breadcrumb 2"
+    },
+    {
+      "id": 1,
+      "label": "Breadcrumb 3"
+    },
+    {
+      "id": 3,
+      "label": "Breadcrumb 4"
+    }
+  ]
+
+  // CHECKBOX
+  modelCheckbox;
+
+  // DROPDOWN-MULTI
+  options = [
+    {
+      "id": 0,
+      "label": "Opzione 1"
+    },
+    {
+      "id": 1,
+      "label": "Opzione 2"
+    },
+    {
+      "id": 2,
+      "label": "Opzione 3"
+    },
+    {
+      "id": 3,
+      "label": "Opzione 4"
+    },
+    {
+      "id": 4,
+      "label": "Opzione 5"
+    },
+    {
+      "id": 5,
+      "label": "Opzione 6"
+    },
+    {
+      "id": 6,
+      "label": "Opzione 7"
+    },
+    {
+      "id": 7,
+      "label": "Opzione 8"
+    },
+    {
+      "id": 8,
+      "label": "Opzione 9"
+    },
+    {
+      "id": 9,
+      "label": "Opzione 10"
+    },
+    {
+      "id": 10,
+      "label": "Opzione 11"
+    }
+  ]
+  modelMulti;
+
+  // DROPDOWN
+  itemsDropdown = [
+    {
+      "id": 1,
+      "label": "test1",
+      "disabled": false
+    },
+    {
+      "id": 2,
+      "label": "test2",
+      "disabled": false
+    },
+    {
+      "id": 3,
+      "label": "test3",
+      "disabled": true
+    }
+  ];
+  selectedItemInput;
+  modelDropdown;
+
+  // INPUT DATE
+  modelDate;
+
+  // INPUT NUMNER
+  modelNumber;
+
+  // INPUT TIME
+  modelTime;
+
+  // RADIO BUTTON
+  modelRadio;
+
+  // STEPPER
+  steps = [
+    {
+      "id": 4,
+      "label": "primo"
+    },
+    {
+      "id": 2,
+      "label": "terzo"
+    },
+    {
+      "id": 1,
+      "label": "Preventivo sostituzione"
+    },
+    {
+      "id": 3,
+      "label": "Materiali sostituiti"
+    },
+    {
+      "id": 5,
+      "label": "Stato impianto"
+    }
+  ];
+
+  // SWITCH
+  modelSwitch;
+
+  // TEXTAREA
+  modelTextarea;
+
+  // TEXTFIELD
+  modelTextfield;
+
+  // NAVIGATION
+  categories = [
+    {
+      "id": "0",
+      "icon": "bu-access-automation",
+      "label": "Access automation"
+    },
+    {
+      "id": "1",
+      "icon": "value-dollaro",
+      "label": "Moneymaker"
+    }
+  ];
+
+  stampa(data) {
+    console.log(data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NavigationModule, ActionPagerModule, SearchModule, TableModule, BreadcrumbsModule,
+         ButtonModule, CheckboxModule, DropdownMultiModule, DropdownModule, HeaderTableModule,
+         IconModule, InputDateModule, InputTimeModule, RadioButtonModule, StepperModule,
+         SwitchModule, TagModule, TextareaModule, TextfieldModule, ToastModule, InputNumberModule } from 'enhancers-design-system';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NavigationModule,
+    ActionPagerModule,
+    SearchModule,
+    TableModule,
+    BreadcrumbsModule,
+    ButtonModule,
+    CheckboxModule,
+    DropdownMultiModule,
+    DropdownModule,
+    HeaderTableModule,
+    IconModule,
+    InputDateModule,
+    InputTimeModule,
+    RadioButtonModule,
+    StepperModule,
+    SwitchModule,
+    TagModule,
+    TextareaModule,
+    TextfieldModule,
+    ToastModule,
+    InputNumberModule
   ],
   providers: [],
   bootstrap: [AppComponent]
