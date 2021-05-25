42fa92390220b74226661b57275d59859541ef88
Mirco Giardina <mirco.giardina@ennova-research.com>
2021-05-25 14:06:19 +0200

    Added accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { NavigationModule, ActionPagerModule, SearchModule, TableModule, BreadcrumbsModule,
          ButtonModule, CheckboxModule, DropdownMultiModule, DropdownModule, HeaderTableModule,
          IconModule, InputDateModule, InputTimeModule, RadioButtonModule, StepperModule,
-         SwitchModule, TagModule, TextareaModule, TextfieldModule, ToastModule, InputNumberModule } from 'enhancers-design-system';
+         SwitchModule, TagModule, TextareaModule, TextfieldModule, ToastModule, InputNumberModule,
+         AccordionModule } from 'enhancers-design-system';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -36,7 +37,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     TextareaModule,
     TextfieldModule,
     ToastModule,
-    InputNumberModule
+    InputNumberModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
